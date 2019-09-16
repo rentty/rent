ac6723a03711697ed19c976c9370bec757f2a636
SKADaddy <692877906@qq.com>
2019-09-16 16:38:32 +0800

    晓清变成DOg

---
+++ src/main/java/com/jl/springboot/bean/Dog.java
@@ -0,0 +1,39 @@
+package com.jl.springboot.bean;
+
+//123
+public class Dog {
+    private String name;
+    private int age;
+
+    public Dog() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "Dog{" +
+                "name='" + name + '\'' +
+                ", age=" + age +
+                '}';
+    }
+
+    public Dog(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
