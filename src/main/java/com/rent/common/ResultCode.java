b28d176fb975760f98fbf1ecf782112bd507b590
SKADaddy <692877906@qq.com>
2019-09-30 09:36:05 +0800

    统一消息返回码和消息描述

---
+++ src/main/java/com/rent/common/ResultCode.java
@@ -0,0 +1,50 @@
+package com.rent.common;
+
+/**
+ * @author BABA
+ * @date 2019/9/30 8:34
+ */
+public enum ResultCode {
+    SUCCESS(true, 20000,"成功"),
+    UNKNOWN_REASON(false, 20001, "未知错误"),
+    BAD_SQL_GRAMMAR(false, 21001, "sql语法错误"),
+    JSON_PARSE_ERROR(false, 21002, "json解析异常"),
+    PARAM_ERROR(false, 21003, "参数不正确"),
+    UNAUTHORIZED(false,21401, "暂未登录或token已经过期");
+
+    private Boolean success; //响应是否成功
+
+    private Integer code; //返回码
+
+    private String message; //返回信息
+
+    ResultCode(Boolean success, Integer code, String message) {
+        this.success = success;
+        this.code = code;
+        this.message = message;
+    }
+
+    public Boolean getSuccess() {
+        return success;
+    }
+
+    public void setSuccess(Boolean success) {
+        this.success = success;
+    }
+
+    public Integer getCode() {
+        return code;
+    }
+
+    public void setCode(Integer code) {
+        this.code = code;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
