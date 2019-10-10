53f9bbed8c302e9f3bb4db6bc6089ca8ecff9437
Rainbowrain15 <1104787489@qq.com>
2019-10-10 16:42:13 +0800

    bean test3

--- src/main/java/com/rent/bean/Entrydl.java
+++ src/main/java/com/rent/bean/Entrydl.java
@@ -43,7 +43,7 @@ public class Entrydl implements Serializable {
         this.edlId = edlId;
         this.dlId = dlId;
         this.edlDetail = edlDetail;
-        //2
+        //3
     }
 
     @Override
