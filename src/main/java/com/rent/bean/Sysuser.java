cb861f6b1075c331a056e92e2ea1a4e6b8f0877c
SKADaddy <692877906@qq.com>
2019-10-17 08:42:10 +0800

    新增后台管理员登录业务

---
+++ src/main/java/com/rent/bean/Sysuser.java
@@ -0,0 +1,8 @@
+package com.rent.bean;
+
+/**
+ * @author BABA
+ * @date 2019/10/16 10:34
+ */
+public class Sysuser {
+}
---
+++ src/main/java/com/rent/controller/SysUserController.java
@@ -0,0 +1,8 @@
+package com.rent.controller;
+
+/**
+ * @author BABA
+ * @date 2019/10/16 9:15
+ */
+public class SysUserController {
+}
